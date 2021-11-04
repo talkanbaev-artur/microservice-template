6f7883fb02a6d4756fefd28fc4f109e63c7df958
EHDW <artur.talkanbaev@gmail.com>
2021-11-04 13:01:51 +0600

    Updated sample application

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func main(){
-	fmt.Println("Hello world")
+	"github.com/gorilla/mux"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Hello World! Your route is %s", vars["key"])
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/{key}", greet)
+
+	http.ListenAndServe(":8000", r)
 }
